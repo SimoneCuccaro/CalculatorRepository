fb93e792a70956a03d42ae0a94cdfae415ee505b
SimoneCuccaro <s.cuccaro1@studenti.unisa.it>
2023-09-26 09:48:07 +0200

    added a new Calculator.java class to this new branch

---
+++ src/main/java/com/example/calculatorexample/Calculator.java
@@ -0,0 +1,18 @@
+package com.example.calculatorexample;
+
+public class Calculator {
+
+    private double a;
+    private double b;
+
+    public Calculator(){
+    }
+
+    public double sum() {
+        return a + b;
+    }
+
+    public double multiply() {
+        return a * b;
+    }
+}
